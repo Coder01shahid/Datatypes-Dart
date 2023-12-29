f8223ada0f6557baa0eae82093e3400c11a1bda9
shahid chaudhary <shahidchaudhary583@gmail.com>
2023-12-29 19:13:12 +0530

    first commit

---
+++ datatypes.dart
@@ -0,0 +1,22 @@
+void main() {
+// now we learn about numbers datatypes in dart
+// 1)int :- it represent whole numbers;
+// 2)double:- it represent float-point-numbers;
+// 3)num :- it represent both types of numbers datatypes like int or double;
+
+  int a = 12;
+  double b = 123.5;
+  // print(a);
+  // print(b);
+  //string :- any combination of alfabets or a text;
+  //boolean :- any value which is identitfy with true or false;
+  //maps :- i don't known yet;
+  //lists :- we can assign multiple values in this datatype and it is same as array;
+  String myname = "shahid";
+  print("hello, $myname");
+  int myAge = 18;
+  print("my age is $myAge");
+  bool answer = myAge == myname;
+  print("that's the truth of your age and name is $answer");
+  //dymanic:- if the datatype of anyvariable is not specified then the datatype is dynamic;
+}
